cdb70d54f27c8eb8a460358a10de3c5882167cde
YasseenR <114093367+YasseenR@users.noreply.github.com>
2025-10-07 12:01:52 -0400

    added first fragment to layout main activity

---
+++ app/src/main/java/edu/temple/realbasicfragmentactivty/ColorFragment.kt
@@ -0,0 +1,25 @@
+package edu.temple.realbasicfragmentactivty
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.graphics.toColorInt
+
+
+class ColorFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_color, container, false).apply {
+            setBackgroundColor(
+                arrayOf("Blue", "Maroon", "Magenta", "Green", "Red").random().toColorInt()
+            )
+        }
+    }
+}
