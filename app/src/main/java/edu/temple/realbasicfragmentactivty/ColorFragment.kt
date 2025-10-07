b48677dd78389129886bfa050c578b6c21366af5
YasseenR <114093367+YasseenR@users.noreply.github.com>
2025-10-07 12:06:31 -0400

    add onclick listener

--- app/src/main/java/edu/temple/realbasicfragmentactivty/ColorFragment.kt
+++ app/src/main/java/edu/temple/realbasicfragmentactivty/ColorFragment.kt
@@ -17,9 +17,11 @@ class ColorFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_color, container, false).apply {
-            setBackgroundColor(
-                arrayOf("Blue", "Maroon", "Magenta", "Green", "Red").random().toColorInt()
-            )
+            setOnClickListener {
+                setBackgroundColor(
+                    arrayOf("Blue", "Maroon", "Magenta", "Green", "Red").random().toColorInt()
+                )
+            }
         }
     }
 }
